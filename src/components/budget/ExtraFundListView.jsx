11474d3383766a59d7d22185fac677dac7090d61
Vusi <vuztec@gmail.com>
2024-07-20 06:49:49 +0200

    EXTRA FUNDS

--- src/components/budget/ExtraFundListView.jsx
+++ src/components/budget/ExtraFundListView.jsx
@@ -8,7 +8,12 @@ import { useQueryClient } from "react-query";
 import ConfirmationDialog from "../Dialogs";
 import axios from "../../config/axios";
 import { handleAxiosResponseError } from "../../utils/handleResponseError";
-import { getFormattedValue, getMonthlyBudgetTotal, getYearlyBudgetTotal } from "../../utils/budget.calculation";
+import {
+  getFormattedValue,
+  getFormattedValueType,
+  getMonthlyBudgetTotal,
+  getYearlyBudgetTotal,
+} from "../../utils/budget.calculation";
 import AddExtraFund from "./AddExtraFund";
 import { formatDate } from "../../utils";
 
@@ -29,7 +34,9 @@ export const ExtraFundListView = ({ gridData }) => {
       .delete(`/api/extra-funds-tracker/${selected}`)
       .then(({ data }) => {
         console.log(data);
-        queryClient.setQueryData(["extrafunds"], (prev) => prev.filter((fund) => fund.id !== selected));
+        queryClient.setQueryData(["extrafunds"], (prev) =>
+          prev.filter((fund) => fund.id !== selected)
+        );
         setOpenDialog(false);
         setIsLoading(false);
       })
@@ -56,17 +63,31 @@ export const ExtraFundListView = ({ gridData }) => {
   const TableHeader = () => (
     <thead>
       <tr className="font-bold bg-black text-white border border-gray-400 text-left text-xs xl:text-sm">
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Name</th>
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Date</th>
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Description</th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Name
+        </th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Date
+        </th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Description
+        </th>
 
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Withdrawal (-)</th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Withdrawal (-)
+        </th>
 
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Credit (+)</th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Credit (+)
+        </th>
 
-        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">Balance</th>
+        <th className="border-l border-gray-300 p-2 text-xs xl:text-sm">
+          Balance
+        </th>
 
-        <th className="p-2 border-l border-gray-300 text-xs xl:text-sm">Actions</th>
+        <th className="p-2 border-l border-gray-300 text-xs xl:text-sm">
+          Actions
+        </th>
       </tr>
     </thead>
   );
@@ -75,7 +96,9 @@ export const ExtraFundListView = ({ gridData }) => {
     <tr className="border border-gray-300 text-sm xl:text-[16px] hover:bg-gray-400/10 text-left">
       <td className="min-w-fit whitespace-nowrap p-2 border-l border-gray-200">
         <div className="flex flex-col items-start gap-1">
-          <span className="flex items-center justify-left gap-2 text-center mb-0 text-gray-900">{record?.Owner}</span>
+          <span className="flex items-center justify-left gap-2 text-center mb-0 text-gray-900">
+            {record?.Owner}
+          </span>
         </div>
       </td>
 
@@ -93,30 +116,52 @@ export const ExtraFundListView = ({ gridData }) => {
 
       <td className="min-w-fit whitespace-nowrap p-2 border-l border-gray-200">
         <div className="flex flex-col items-start gap-1">
-          <p className="text-black">{getFormattedValue(user, record?.Amount, record?.Type, "withdrawal")}</p>
+          <p className="text-black">
+            {getFormattedValueType(
+              user,
+              record?.Amount,
+              record?.Type,
+              "Withdrawal"
+            )}
+          </p>
         </div>
       </td>
 
       <td className="min-w-fit whitespace-nowrap p-2 border-l border-gray-200">
         <div className="flex flex-col items-start gap-1">
-          <p className="text-black">{getFormattedValue(user, record?.Amount, record?.Type, "credit")}</p>
+          <p className="text-black">
+            {getFormattedValueType(
+              user,
+              record?.Amount,
+              record?.Type,
+              "Credit"
+            )}
+          </p>
         </div>
       </td>
       <td className="min-w-fit whitespace-nowrap p-2 border-l border-gray-200">
         <div className="flex flex-col items-start gap-1">
-          <p className="text-black">{getFormattedValue(user, record?.MonthlyBudget)}</p>
+          <p className="text-black">
+            {getFormattedValue(user, record?.MonthlyBudget)}
+          </p>
         </div>
       </td>
 
       <td className="min-w-max p-2 border-l border-r border-gray-200">
         <div className="flex items-center text-left gap-3 justify-start">
           <FaEdit
-            className={clsx(`text-editcolor`, "hover:text-orange-500 font-semibold cursor-pointer sm:px-0")}
+            className={clsx(
+              `text-editcolor`,
+              "hover:text-orange-500 font-semibold cursor-pointer sm:px-0"
+            )}
             onClick={() => editClick(record)}
           />
 
           <RiDeleteBin2Fill
-            className={clsx(`text-deletecolor`, "hover:text-red-500 font-semibold cursor-pointer sm:px-0")}
+            className={clsx(
+              `text-deletecolor`,
+              "hover:text-red-500 font-semibold cursor-pointer sm:px-0"
+            )}
             onClick={() => deleteClick(record.id)}
           />
         </div>
@@ -130,7 +175,9 @@ export const ExtraFundListView = ({ gridData }) => {
 
       <td className="min-w-fit whitespace-nowrap p-3 border-gray-200"></td>
 
-      <td className="min-w-fit whitespace-nowrap p-3 border-l border-gray-200">TOTAL</td>
+      <td className="min-w-fit whitespace-nowrap p-3 border-l border-gray-200">
+        TOTAL
+      </td>
 
       <td className="min-w-fit whitespace-nowrap p-3 border-l border-gray-200">
         <div className="flex flex-col items-start gap-1">
@@ -162,7 +209,9 @@ export const ExtraFundListView = ({ gridData }) => {
             <table className="w-[97%] ml-5 -mb-5">
               <thead>
                 <tr>
-                  <th className="p-2 w-full uppercase bg-black text-white flex items-center justify-center">EXTRA FUND TRACKER</th>
+                  <th className="p-2 w-full uppercase bg-black text-white flex items-center justify-center">
+                    EXTRA FUND TRACKER
+                  </th>
                 </tr>
               </thead>
             </table>
@@ -179,8 +228,18 @@ export const ExtraFundListView = ({ gridData }) => {
         </div>
       </div>
 
-      <AddExtraFund open={open} setOpen={setOpen} recordData={selected} key={new Date().getTime().toString()} />
-      <ConfirmationDialog isLoading={isLoading} open={openDialog} setOpen={setOpenDialog} onClick={() => deleteHandler(selected)} />
+      <AddExtraFund
+        open={open}
+        setOpen={setOpen}
+        recordData={selected}
+        key={new Date().getTime().toString()}
+      />
+      <ConfirmationDialog
+        isLoading={isLoading}
+        open={openDialog}
+        setOpen={setOpenDialog}
+        onClick={() => deleteHandler(selected)}
+      />
     </>
   );
 };
