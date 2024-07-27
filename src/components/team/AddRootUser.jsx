59a098a79c914a10b8d0a2cf197bfa17a0773132
Azam Dildar <azamdev00@gmail.com>
2024-07-27 11:07:41 +0500

    update

--- src/components/team/AddRootUser.jsx
+++ src/components/team/AddRootUser.jsx
@@ -25,11 +25,7 @@ export const AddRootUser = ({ open, setOpen }) => {
     label: format.label,
   }));
 
-  const [countryCode, setCountryCode] = useState(
-    user
-      ? CountryData.find((country) => country.name === user.Country)?.isoCode
-      : ""
-  );
+  const [countryCode, setCountryCode] = useState(user ? CountryData.find((country) => country.name === user.Country)?.isoCode : "");
 
   const navigate = useNavigate();
 
@@ -50,15 +46,12 @@ export const AddRootUser = ({ open, setOpen }) => {
     if (user?.id) {
       setValue("FullName", user.FullName);
       setValue("Email", user.Email);
-      setValue("Contact", user.Contact);
       setValue("Currency", user.Currency);
       setValue("PartnerAge", user.PartnerAge);
       setValue("SelfAge", user.SelfAge);
       setValue("Country", user.Country);
 
-      const country = CountryData.find(
-        (country) => country.name === user?.Country
-      )?.isoCode;
+      const country = CountryData.find((country) => country.name === user?.Country)?.isoCode;
       setCountryCode(country);
     }
 
@@ -69,14 +62,11 @@ export const AddRootUser = ({ open, setOpen }) => {
   const handleOnSubmit = async (data) => {
     setIsLoading(() => true);
 
-    const id = toast.loading("Loading....");
+    const id = toast.loading("Updating User Profile....");
     delete data.ConfirmPassword;
-
-    console.log(data);
 
     if (user?.id) {
       delete data.Password;
-      console.log(data);
       axios
         .patch("/api/rootusers/" + user.id, data)
         .then(({ data }) => {
@@ -132,9 +122,7 @@ export const AddRootUser = ({ open, setOpen }) => {
   const handleCountryChange = (e) => {
     if (e && e.target?.value) {
       const selCountry = e.target?.value;
-      const targetCountry = CountryData.find(
-        (item) => item?.isoCode === selCountry
-      );
+      const targetCountry = CountryData.find((item) => item?.isoCode === selCountry);
       setValue("Country", targetCountry.name);
       setValue("Currency", targetCountry.currency);
       const selCountryCode = targetCountry ? targetCountry.isoCode : "";
@@ -148,10 +136,7 @@ export const AddRootUser = ({ open, setOpen }) => {
     <>
       <ModalWrapper open={open} setOpen={setOpen}>
         <form onSubmit={handleSubmit(handleOnSubmit)} className="">
-          <Dialog.Title
-            as="h2"
-            className="text-base font-bold leading-6 text-gray-900 mb-4"
-          >
+          <Dialog.Title as="h2" className="text-base font-bold leading-6 text-gray-900 mb-4">
             {user ? "UPDATE USER PROFILE" : "CREATE NEW ACCOUNT"}
           </Dialog.Title>
           <div className="mt-2 flex flex-col gap-6 overflow-y-scroll bg-scroll">
@@ -170,8 +155,7 @@ export const AddRootUser = ({ open, setOpen }) => {
                 />
               </div>
               <p className="text-[11px] text-gray-500">
-                NOTE: Can deduct the years from your age if you didn't start
-                working until you completed an advanced degree, served in the
+                NOTE: Can deduct the years from your age if you didn't start working until you completed an advanced degree, served in the
                 military or were disabled.
               </p>
               <div className="w-full">
@@ -183,8 +167,7 @@ export const AddRootUser = ({ open, setOpen }) => {
                   className="w-full rounded"
                   register={register("SelfAge", {
                     valueAsNumber: true,
-                    validate: (value) =>
-                      value >= 0 || "Age must be zero or positive",
+                    validate: (value) => value >= 0 || "Age must be zero or positive",
                   })}
                   error={errors.SelfAge ? errors.SelfAge.message : ""}
                 />
@@ -198,8 +181,7 @@ export const AddRootUser = ({ open, setOpen }) => {
                   className="w-full rounded"
                   register={register("PartnerAge", {
                     valueAsNumber: true,
-                    validate: (value) =>
-                      value >= 0 || "Age must be zero or positive",
+                    validate: (value) => value >= 0 || "Age must be zero or positive",
                   })}
                   error={errors.PartnerAge ? errors.PartnerAge.message : ""}
                 />
@@ -260,12 +242,9 @@ export const AddRootUser = ({ open, setOpen }) => {
                   className="w-full rounded"
                   register={register("ConfirmPassword", {
                     required: "Confirm Password is required!",
-                    validate: (value) =>
-                      value === Password || "Passwords do not match",
+                    validate: (value) => value === Password || "Passwords do not match",
                   })}
-                  error={
-                    errors.ConfirmPassword ? errors.ConfirmPassword.message : ""
-                  }
+                  error={errors.ConfirmPassword ? errors.ConfirmPassword.message : ""}
                 />
               </div>
             )}
