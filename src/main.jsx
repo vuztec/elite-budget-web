f4fad3eb040d070b70d9ae1b8671a7d0485f47e6
Azam Dildar <azamdev00@gmail.com>
2024-08-17 10:22:32 +0500

    remove syncfusion

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import { registerLicense } from "@syncfusion/ej2-base";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-registerLicense("ORg4AjUWIQA/Gnt2V1hhQlJAfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5bdkdiUH1ecXNTQmlc");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
 //     <React.StrictMode>
 // </React.StrictMode>
