45c295677dcbbe5ac5e3ce3cdaf233230eab542c
Azam Dildar <azamdev00@gmail.com>
2024-09-02 11:01:23 +0500

    auto renewal

--- src/pages/subscription/Subscription.jsx
+++ src/pages/subscription/Subscription.jsx
@@ -200,9 +200,14 @@ export const Subscription = () => {
               <tr>
                 <td className="min-w-fit whitespace-nowrap p-2">Auto Renewal</td>
                 <td className="min-w-fit whitespace-nowrap p-2 border-l border-gray-200 font-bold">
-                  <label class="inline-flex items-center cursor-pointer">
-                    <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} class="sr-only peer" />
-                    <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-600"></div>
+                  <label className="inline-flex items-center cursor-pointer">
+                    <input
+                      type="checkbox"
+                      checked={isChecked}
+                      onChange={handleCheckboxChange}
+                      className="sr-only peer"
+                    />
+                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-gray-600"></div>
                   </label>
                 </td>
               </tr>
