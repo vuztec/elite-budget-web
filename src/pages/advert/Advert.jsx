0cbcd95cf929c64999811140aaaee76eaadd6925
Azam Dildar <azamdev00@gmail.com>
2024-10-14 10:33:25 +0500

    update

--- src/pages/advert/Advert.jsx
+++ src/pages/advert/Advert.jsx
@@ -12,8 +12,15 @@ import Hero from '../../components/advert/hero/Hero';
 import ThreeCards from '../../components/advert/three-cards/ThreeCards';
 import Footer from '../../components/advert/footer/Footer';
 import Budget from '../../components/advert/budget/Budget';
+import { useState } from 'react';
+import ModalWrapper from '../../components/ModalWrapper';
 
 export const Advert = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleModal = () => {
+    setOpen((prev) => !prev);
+  };
   return (
     <>
       <div className="w-full bg-white">
@@ -33,6 +40,9 @@ export const Advert = () => {
                 streamlines the budget management process in one easy-to-use platform.
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -69,6 +79,9 @@ export const Advert = () => {
                 streamlines the budget management process in one easy-to-use platform.
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -103,6 +116,9 @@ export const Advert = () => {
                 or overlooked
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -135,6 +151,9 @@ export const Advert = () => {
                 Report (what you own less what you owe){' '}
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -171,6 +190,9 @@ export const Advert = () => {
                 dates calendar for automatic savings (or debt reduction)
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -203,6 +225,9 @@ export const Advert = () => {
                 expense split calculator when couples wish to split their joint costs{' '}
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -236,6 +261,9 @@ export const Advert = () => {
                 funds tracker to manage the budget buffer (the money that exceeds your budget){' '}
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -268,6 +296,9 @@ export const Advert = () => {
                 registers so you can enter your bank transactions and reconcile your bank accounts{' '}
               </li>
             </ul>
+            <button className="flex hover:underline text-blue-500" onClick={handleModal}>
+              show more
+            </button>
             <a
               href="/login"
               className="hidden lg:flex w-fit items-center gap-2 text-lg lg:text-xl font-medium py-3 px-4 bg-white shadow-md text-black rounded-md"
@@ -292,6 +323,43 @@ export const Advert = () => {
         <ThreeCards />
         <Footer />
       </div>
+
+      <ModalWrapper open={open} handleClose={handleModal}>
+        <ul className="ml-12 list-disc">
+          <li>Step one is to enter all your data. That’s the hardest part but just for the first time!</li>
+          <li>All reports and accounts are available by Individual, Partner, Joint or Household.</li>
+          <li>
+            The app includes budgeting tool add-ons:
+            <ul className="list-disc ml-6">
+              <li>
+                <span className="font-bold"> Extra pay dates calendar</span> for automatic savings (or debt reduction)
+              </li>
+              <li>
+                <span className="font-bold">A handy checklist</span> to use when you enter your budget items into your
+                checkbook register so nothing is forgotten or overlooked
+              </li>
+              <li>
+                {' '}
+                <span className="font-bold">Joint expense split calculator</span> when couples wish to split their joint
+                costs
+              </li>
+              <li>
+                <span className="font-bold">An extra funds tracker</span> to manage the budget buffer (the money that
+                exceeds your budget)
+              </li>
+              <li>
+                <span className="font-bold">Bank account registers</span> so you can enter your bank transactions and
+                reconcile your bank accounts
+              </li>
+              <li>
+                <span className="font-bold">Net worth Report</span> (what you own less what you owe)
+              </li>
+            </ul>
+          </li>
+          <li>Easy to make changes</li>
+          <li>All reports are automatically updated instantly.</li>
+        </ul>
+      </ModalWrapper>
     </>
   );
 };
