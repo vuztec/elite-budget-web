260860099f467cc497a70e00af660b47b6510743
Vusi <vuztec@gmail.com>
2025-08-22 06:29:42 +0200

    updated code

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: '/',
   build: {
-    outDir: "dist", // Change if necessary, default is 'dist'
+    outDir: 'dist', // Change if necessary, default is 'dist'
   },
   preview: {
     port: 3000,
@@ -16,6 +16,11 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
+  },
+  define: {
+    __VUE_OPTIONS_API__: true,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
   },
 });
